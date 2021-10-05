806ccf9d195b69574ada9660f1107d554968a83f
Federico Frenguelli <synasius@gmail.com>
2021-10-05 10:12:13 +0200

    [brave] Added brave browser setup

--- gru.fish
+++ gru.fish
@@ -218,3 +218,16 @@ if read_confirm "Install/Upgrade Rust"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path
   end
 end
+
+
+if read_confirm "Install Brave Browser"
+  sudo apt install apt-transport-https curl
+
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+  sudo apt update
+
+  sudo apt install brave-browser
+end
