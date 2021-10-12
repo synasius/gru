eb5cdb928e1d54523d79abd38194efebd07aebfb
Federico Frenguelli <synasius@gmail.com>
2021-10-12 08:35:18 +0200

    Added Github CLI setup

--- gru.fish
+++ gru.fish
@@ -121,6 +121,20 @@ if read_confirm "Setup Google Cloud SDK and tools"
 end
 
 
+if read_confirm "Setup github CLI"
+  if test ! -f /etc/apt/sources.list.d/github-cli.list
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch="(dpkg --print-architecture)" signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt install gh
+
+    gh auth login
+    gh config set editor nvim
+  end
+  gh completion -s fish > $HOME/.config/fish/completions/gh.fish
+end
+
+
 if read_confirm "Install Unity dev environment"
   sudo snap install code --classic
 
