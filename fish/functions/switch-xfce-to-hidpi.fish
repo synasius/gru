91327ab37ed31f1057c44d08db8f8f2e165617a6
Federico Frenguelli <synasius@gmail.com>
2022-02-11 09:07:21 +0100

    Added some tweaks to the functions to switch from and to hdpi

--- fish/functions/switch-xfce-to-hidpi.fish
+++ fish/functions/switch-xfce-to-hidpi.fish
@@ -1,4 +1,6 @@
 function switch-xfce-to-hidpi
   xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
   xfconf-query -c xfwm4 -p /general/theme -s Arc-Darker-hdpi
+  xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 48
+  xfce4-panel -r
 end
