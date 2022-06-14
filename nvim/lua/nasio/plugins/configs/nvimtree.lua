1acf7ed0c7a742a5bdfd0512f62b70593e99c5a3
Federico Frenguelli <synasius@gmail.com>
2022-06-14 17:40:14 +0200

    [neovim] Fixed nvim-tree config

--- nvim/lua/nasio/plugins/configs/nvimtree.lua
+++ nvim/lua/nasio/plugins/configs/nvimtree.lua
@@ -1,11 +1,3 @@
-vim.g.nvim_tree_git_hl = 1
-
-vim.g.nvim_tree_show_icons = {
-	git = 1,
-	folders = 1,
-	files = 1,
-}
-
 require("nvim-tree").setup({
 	git = {
 		ignore = true,
@@ -15,8 +7,16 @@ require("nvim-tree").setup({
 		dotfiles = true,
 	},
 	renderer = {
+		highlight_git = true,
 		indent_markers = {
 			enable = true,
+		},
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				git = true,
+			},
 		},
 	},
 })
