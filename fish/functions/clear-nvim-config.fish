52481a2dd0ee2bbf1fd41613d207b28661e51d8c
Federico Frenguelli <synasius@gmail.com>
2022-12-13 08:51:26 +0100

    Added logging to clear-nvim-config function

--- fish/functions/clear-nvim-config.fish
+++ fish/functions/clear-nvim-config.fish
@@ -1,5 +1,21 @@
-function clear-nvim-config --description 'clear nvim config'
-unlink ~/.config/nvim
-rm -rf ~/.local/share/nvim
-rm -rf ~/.cache/nvim
+function clear-nvim-config --description 'clear nvim config' -a remove_config
+  echo "Removing ~/.local/share/nvim"
+  rm -rf ~/.local/share/nvim
+  echo "Removing ~/.cache/nvim"
+  rm -rf ~/.cache/nvim
+
+
+  if test -n $remove_config
+    set -l nvim_config_dir "~/.config/nvim"
+
+    if test -L $nvim_config_dir
+      echo "Unlink $nvim_config_dir"
+      unlink $nvim_config_dir
+    end
+
+    if test -d $nvim_config_dir
+      echo "Remove $nvim_config_dir"
+      rm -rf $nvim_config_dir
+    end
+  end
 end
