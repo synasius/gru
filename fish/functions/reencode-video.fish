34eaa8d34f7538a78595da00d6ecfe176f7aca95
sinasio <synasius@gmail.com>
2024-09-19 13:08:41 +0200

    Improved reencode video

--- fish/functions/reencode-video.fish
+++ fish/functions/reencode-video.fish
@@ -1,14 +1,8 @@
 function reencode-video --description 'Reencode video with proper compression and codec' --argument extension
-    mkdir -p old
     for f in (ls **/*.$extension)
         set -l basname (basename $f .$extension)
         set -l dirnam (dirname $f)
 
-        if [ $dirnam != "." ]
-            mkdir -p old/$dirnam
-        end
-
-        mv $f old/$f
-        ffmpeg -i old/$f -c:v libx265 -crf 28 -preset medium -vf format=yuv420p -c:a aac -map_metadata 0 -movflags +faststart $dirnam/$basname.mp4
+        ffmpeg -i $f -c:v libx265 -crf 28 -preset medium -vf format=yuv420p -c:a aac -map_metadata 0 -movflags +faststart $dirnam/$basname".enc.mp4"
     end
 end
