2461ebbdc9a092f37ed06978ae13959c28dd9224
sinasio <synasius@gmail.com>
2025-02-28 09:39:50 +0100

    Changed theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,19 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.default_prog = { '/usr/bin/fish' }
+config.default_prog = { "/usr/bin/fish" }
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Dracula (Official)"
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
 
-config.font = wezterm.font 'MonoLisa'
+config.font = wezterm.font("MonoLisa")
 
 -- and finally, return the configuration to wezterm
 return config
